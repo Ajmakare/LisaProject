6ff38ee0e354ec8f2951ca60c42be72d1a39623a
Aidan Makare <aidanmakaree@gmail.com>
2023-02-20 18:02:14 -0600

    configure static f

--- LisaProject/settings.py
+++ LisaProject/settings.py
@@ -157,10 +157,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR / "LisaApp", 'staticfiles')
+STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / "LisaApp/static",
     BASE_DIR / "LisaApp/static/Media",
+    STATIC_ROOT,
 ]
 
 # Default primary key field type
