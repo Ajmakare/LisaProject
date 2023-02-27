a9fb1c7f63a141444434e02d28d31d97790f4cfe
Aidan Makare <aidanmakaree@gmail.com>
2023-02-27 15:49:24 -0600

    settings change

--- LisaProject/settings.py
+++ LisaProject/settings.py
@@ -193,8 +193,12 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = env('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')    
+
+if not IS_HEROKU:
+    EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
 
 PAYPAL_RECEIVER_EMAIL = 'easydoesit2383@gmail.com'
 
