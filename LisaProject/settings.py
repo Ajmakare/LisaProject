26bbfb07de176c62107c1ab2462f748ef3ecd7f2
Aidan Makare <aidanmakaree@gmail.com>
2023-02-27 15:40:10 -0600

    gmail smtp

--- LisaProject/settings.py
+++ LisaProject/settings.py
@@ -190,8 +190,8 @@ if "CI" in os.environ:
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = 'smtp.mail.yahoo.com'
-EMAIL_PORT = 465
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = env('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
